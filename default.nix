5be049253e10e9b7adca3f385c5e3da25d95fe85
Sean Hickman <sean@grumply.com>
2020-02-13 23:33:40 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure, pure-cond, pure-prop, pure-proxy, pure-txt, pure-transition, stdenv }:
 mkDerivation {
   pname = "pure-portal";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure pure-cond pure-prop pure-proxy pure-txt pure-transition ];
   homepage = "github.com/grumply/pure-portal";
